2e4e6fb5ad85c52d8149c22a39393c4268ed344c
RogelioTeranSuarez <20151602@aguascalientes.tecnm.mx>
2024-04-12 13:36:16 -0600

    Final

--- src/diccionariodecontrasenas/Main.java
+++ src/diccionariodecontrasenas/Main.java
@@ -1,30 +1,42 @@
 
 package diccionariodecontrasenas;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
-    
     final static String[] CHARLIST = {"A","B","C","D","E","F","G","H","I","J",
                                       "K","L","M","N","Ñ","O","P","Q","R","S",
                                       "T","U","V","W","X","Y","Z","0","1","2",
                                       "3","4","5","6","7","8","9"};
-    static int contador = 1;
+    static FileWriter writer;
+    static int counter = 1; // Contador para el formato [n]
     
     public static void main(String[] args) {
-        int longitudDeContraseña = 4; // Longitud de la combinación
-        generarDiccionario(longitudDeContraseña, "");
+        int combinationLength = 5; // Longitud de la combinación
+        try {
+            writer = new FileWriter("combinations.txt");
+            generateCombinations(combinationLength, "");
+            writer.close();
+        } catch (IOException e) {
+            System.err.println("Error al escribir en el archivo: " + e.getMessage());
+        }
     }
     
-    public static void generarDiccionario(int length, String prefix) {
+    public static void generateCombinations(int length, String prefix) {
         if (length == 0) {
-            System.out.println("["+contador+"]"+prefix);
-            contador++;
+            try {
+                writer.write("[" + counter + "]" + prefix + "\n");
+                counter++;
+            } catch (IOException e) {
+                System.err.println("Error al escribir en el archivo: " + e.getMessage());
+            }
             return;
         }
         
         for (int i = 0; i < CHARLIST.length; i++) {
             String newPrefix = prefix + CHARLIST[i];
-            generarDiccionario(length - 1, newPrefix);
+            generateCombinations(length - 1, newPrefix);
         }
     }
 }
-
